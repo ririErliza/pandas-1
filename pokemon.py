d3c8b359990ad8d77b8ffac53ab9922175c41926
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-24 15:07:09 +0200

    update

--- pokemon.py
+++ pokemon.py
@@ -7,3 +7,36 @@ df = pd.read_csv('pokemon_data.csv')
 print(df.head())
 
 print(df)
+
+print(df.tail(3))
+
+
+# dfTxt = pd.read_csv('pokemon_data.txt', delimiter='\t')
+# print(dfTxt)
+
+
+#'''  Read headers  '''
+print(df.columns)
+
+
+#'''  Read each column  '''
+print(df['Name'])
+
+print(df[['Name', 'Type 1', 'HP']])
+
+
+#'''  Read each row  '''
+print(df.head(4))
+
+print(df.iloc[5])
+
+print(df.iloc[11:14])
+
+for index, row in df.iterrows():
+    print(index, row['Name'])
+
+print(df.loc)
+
+#''' Read a specific location (R,C)'''
+print(df.iloc[2,1]) #Venusaur
+
